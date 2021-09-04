bba6c29a1b61d0fcf19cb76aa971532183409669
Yogesh Sajanikar <yogesh_sajanikar@yahoo.com>
2021-09-04 22:58:24 +0530

    Add SQL Types
    - Add type for "SELECT" and "JOIN"

---
+++ libs/Institis.SQL.Types/Convert.fs
@@ -0,0 +1,10 @@
+namespace Institis.SQL.Types
+
+    open System
+    open Institis.SQL.Types.Data
+
+
+    module Conversion =
+        
+        let toSQL (sql: SQL) : string = raise (NotImplementedException())
+
